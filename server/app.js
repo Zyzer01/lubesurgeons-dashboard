9be93db6cdbce4e4bfc1d4fd50899ae401d72252
David Onifade <davidicfola@gmail.com>
2023-11-13 23:21:34 +0000

    Edit Cross origin port

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 // app.set('view engine', 'handlebars');
 
 const corsOptions = {
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:3001'],
   methods: ['GET', 'POST'], 
 };
 
