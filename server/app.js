f2d599a90d09d7858608f266fc417c895c01660a
David Onifade <davidicfola@gmail.com>
2023-11-13 23:20:31 +0000

    Update app.js

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 
 const corsOptions = {
   origin: ['http://localhost:5173'],
-  methods: ['GET', 'POOST'],
+  methods: ['GET', 'POST'],
 };
 
 app.use(cors(corsOptions));
