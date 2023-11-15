255aa8f94e1ce37e30c253a9b7f3d4064b10c33a
David Onifade <davidicfola@gmail.com>
2023-11-15 23:26:10 +0000

    label tag edit

--- src/components/SwitcherFour.tsx
+++ src/components/SwitcherFour.tsx
@@ -5,7 +5,7 @@ const SwitcherFour = () => {
 
   return (
     <div>
-      <labeli
+      <label
         htmlFor="toggle4"
         className="flex cursor-pointer select-none items-center"
       >
